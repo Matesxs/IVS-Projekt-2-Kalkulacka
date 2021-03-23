7c5e93514c42c030aec2e518fd90f8a4578936be
Martin Dousa <martindousa186@gmail.com>
2021-03-23 18:27:36 +0100

    created placeholders for all math functions

--- src/mathLib/basics/math_functions.py
+++ src/mathLib/basics/math_functions.py
@@ -2,7 +2,48 @@
 # @package math_functions
 #
 
+import math
+
 ##
 # @brief Holds all mathematic functions of library
 #
 class MathFunctions:
+  @staticmethod
+  def add_operation(num1, num2):
+    pass
+
+  @staticmethod
+  def sub_operation(num1, num2):
+    pass
+
+  @staticmethod
+  def multiply_operation(num1, num2):
+    pass
+
+  @staticmethod
+  def divide_operation(num1, num2):
+    pass
+
+  @staticmethod
+  def power_operation(num1, num2):
+    pass
+
+  @staticmethod
+  def root_operation(n, value):
+    pass
+
+  @staticmethod
+  def invert_operation(num):
+    pass
+
+  @staticmethod
+  def factorial_operation(num):
+    pass
+
+  @staticmethod
+  def natural_log_operation(num):
+    pass
+
+  @staticmethod
+  def abs_operation(num):
+    pass
