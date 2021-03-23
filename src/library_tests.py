5d32e662ca06e4ebcec24214d6388ea6a2c95fc1
Martin Dousa <martindousa186@gmail.com>
2021-03-23 18:44:34 +0100

    add entrypoint for testing code coverage with coverage

--- src/library_tests.py
+++ src/library_tests.py
@@ -9,3 +9,10 @@ from mathLib import MathFunctions
 # @brief Test mathematic operations of math library
 #
 class MathLibTestBasicFunctions(unittest.TestCase):
+  pass
+
+if __name__ == '__main__':
+  try:
+    unittest.main()
+  except Exception as e:
+    print(f"Compiling of test failed\n{e}")
