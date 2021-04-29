1dac4990336866a6a23f953946cbf21d0fbd0999
Martin Dousa <martindousa186@gmail.com>
2021-04-30 00:03:15 +0200

    added implementation of entrypoint

--- src/mathLib/entry_point.py
+++ src/mathLib/entry_point.py
@@ -2,10 +2,42 @@
 # @package entry_point
 #
 
+from .tokenizer import Tokenizer
+from .prsr import Parser
+from .interpreter import Interpreter
+from .basics.number_class import Number
+
 ##
 # @brief Entrypoint for UI
 #
 # @param test String input to interpret
 #
 def interpret_text_input(text:str):
-  pass
+  tokenizer = Tokenizer(text)
+
+  try:
+    tokens = tokenizer.tokenize()
+  except:
+    return "Error"
+
+  if not tokens:
+    return "0"
+
+  # print(f"Debug Tokens: {tokens}")
+  parser = Parser(tokens)
+  try:
+    node_tree = parser.parse()
+  except:
+    return "Error"
+
+  if not node_tree:
+    return "0"
+
+  # print(f"Debug Nodes: {node_tree}")
+  interpreter = Interpreter()
+  try:
+    value:Number = interpreter.interpret(node_tree)
+  except:
+    return "Error"
+
+  return str(value)
