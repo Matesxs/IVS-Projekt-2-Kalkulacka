105507f475c1a301758ec262f93d8ff0f7c92c7e
SchindlerV <64971365+SchindlerV@users.noreply.github.com>
2021-04-29 22:58:45 +0200

    Update library_tests.py

--- src/library_tests.py
+++ src/library_tests.py
@@ -4,7 +4,7 @@
 
 import unittest
 import math
-from mathLib import MathFunctions, Interpreter, Parser, Tokenizer, Token, TokenType
+from mathLib import MathFunctions, Interpreter, Parser, Tokenizer, Token, TokenType, Number
 from mathLib.basics.nodes import *
 from mathLib.entry_point import interpret_text_input
 
@@ -12,7 +12,10 @@ from mathLib.entry_point import interpret_text_input
 # @brief Test mathematic operations of math library
 #
 class MathLibTestBasicFunctions(unittest.TestCase):
-  def test_add(self):  # add function tests
+  ##
+  # @brief add function tests
+  #
+  def test_add(self):  
     self.assertEqual(MathFunctions.add_operation(0, 1), 1)
     self.assertEqual(MathFunctions.add_operation(0, 0), 0)
     self.assertEqual(MathFunctions.add_operation(1, 0), 1)
@@ -20,8 +23,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertEqual(MathFunctions.add_operation(-1, 0), -1)
     self.assertEqual(MathFunctions.add_operation(1.5, 2.3), 3.8)
     self.assertEqual(MathFunctions.add_operation(100000, 900000), 1000000)
-
-  def test_sub(self):  # subtraction function tests
+  ##
+  # @brief subtraction function tests
+  #
+  def test_sub(self):  
     self.assertEqual(MathFunctions.sub_operation(1, 1), 0)
     self.assertEqual(MathFunctions.sub_operation(0, 1), -1)
     self.assertEqual(MathFunctions.sub_operation(1, 0), 1)
@@ -30,8 +35,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertEqual(MathFunctions.sub_operation(5, -6), 11)
     self.assertAlmostEqual(MathFunctions.sub_operation(-5.3, -4.1), -1.2)
     self.assertEqual(MathFunctions.sub_operation(100, 5000000), -4999900)
-
-  def test_mul(self):  # multiplication function tests
+  ##
+  # @brief multiplication function tests
+  #
+  def test_mul(self):  
     self.assertEqual(MathFunctions.multiply_operation(1, 1), 1)
     self.assertEqual(MathFunctions.multiply_operation(1, 0), 0)
     self.assertEqual(MathFunctions.multiply_operation(0, 1), 0)
@@ -41,8 +48,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertEqual(MathFunctions.multiply_operation(-5, 3), -15)
     self.assertEqual(MathFunctions.multiply_operation(-70, -33), 2310)
     self.assertEqual(MathFunctions.multiply_operation(-1450.1444, 789444784.8875), -1144808933913.812755)
-
-  def test_div(self):  # division function tests
+  ##
+  # @brief division function tests
+  #
+  def test_div(self):  
     self.assertEqual(MathFunctions.divide_operation(1, 1), 1)
     self.assertEqual(MathFunctions.divide_operation(0, 1), 0)
     self.assertEqual(MathFunctions.divide_operation(5, 5), 1)
@@ -52,8 +61,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertEqual(MathFunctions.divide_operation(25, 4), 6.25)
     self.assertAlmostEqual(MathFunctions.divide_operation(159, 753), 0.21115537848, 10)
     self.assertAlmostEqual(MathFunctions.divide_operation(753, -159), -4.73584905660, 10)
-
-  def test_div_by_zero(self):  # testing if division by 0 raises error
+  ##
+  # @brief testing if division by 0 raises error
+  #
+  def test_div_by_zero(self):  
     with self.assertRaises(ZeroDivisionError):
       MathFunctions.divide_operation(0, 0)
     with self.assertRaises(ZeroDivisionError):
@@ -63,7 +74,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(ZeroDivisionError):
       MathFunctions.divide_operation(-485.33, 0)
 
-  def test_pow(self):  # power function tests
+  ##
+  # @brief power function tests
+  #
+  def test_pow(self):  
     self.assertEqual(MathFunctions.power_operation(1, 1), 1)
     self.assertEqual(MathFunctions.power_operation(0, 10), 0)
     self.assertEqual(MathFunctions.power_operation(10, 0), 1)
@@ -76,7 +90,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertEqual(MathFunctions.power_operation(-5, 8), 390625)
     self.assertAlmostEqual(MathFunctions.power_operation(-80, -8), 0.000000000000000596046, 20)
 
-  def test_root(self):  # root function tests
+  ##
+  # @brief root function tests
+  #
+  def test_root(self):  
     self.assertEqual(MathFunctions.root_operation(1, 1), 1)
     self.assertEqual(MathFunctions.root_operation(1, 5), 5)
     self.assertAlmostEqual(MathFunctions.root_operation(10, 5), 1.17461894308, 10)
@@ -86,7 +103,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     self.assertAlmostEqual(MathFunctions.root_operation(1.2, 3.4), 2.77267583598, 10)
     self.assertAlmostEqual(MathFunctions.root_operation(-5.6, 7.8), 0.69294280921, 10)
 
-  def test_root_of_neg_number(self):  # testing if root of negative numbers raises error
+  ##
+  # @brief testing if root of negative numbers raises error
+  #
+  def test_root_of_neg_number(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.root_operation(2, -5)
     with self.assertRaises(RuntimeError):
@@ -96,7 +116,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(RuntimeError):
       MathFunctions.root_operation(-8.3, -5.778)
 
-  def test_zeroth_root(self):  # testing if 0th root raises error
+  ##
+  # @brief testing if 0th root raises error
+  #
+  def test_zeroth_root(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.root_operation(0, 1)
     with self.assertRaises(RuntimeError):
@@ -104,18 +127,27 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(RuntimeError):
       MathFunctions.root_operation(0, 55.5)
 
-  def test_log(self):  # natural logarithm function tests
+  ##
+  # @brief natural logarithm function tests
+  #
+  def test_log(self):  
     self.assertEqual(MathFunctions.natural_log_operation(1), 0)
     self.assertAlmostEqual(MathFunctions.natural_log_operation(2), 0.69314718056, 10)
     self.assertAlmostEqual(MathFunctions.natural_log_operation(3.4), 1.22377543162, 10)
     self.assertAlmostEqual(MathFunctions.natural_log_operation(100), 4.60517018599, 10)
     self.assertAlmostEqual(MathFunctions.natural_log_operation(100100100100), 25.3294365233, 10)
 
-  def test_zero_log(self):  # testing if natural logarithm of 0 raises error
+  ##
+  # @brief testing if natural logarithm of 0 raises error
+  #
+  def test_zero_log(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.natural_log_operation(0)
 
-  def test_neg_log(self):  # testing if natural logarithm of negative numbers raises error
+  ##
+  # @brief testing if natural logarithm of negative numbers raises error
+  #
+  def test_neg_log(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.natural_log_operation(-1)
     with self.assertRaises(RuntimeError):
@@ -123,13 +155,19 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(RuntimeError):
       MathFunctions.natural_log_operation(-45.44)
 
-  def test_fac(self):  # factorial function tests
+  ##
+  # @brief factorial function tests
+  #
+  def test_fac(self):  
     self.assertEqual(MathFunctions.factorial_operation(0), 1)
     self.assertEqual(MathFunctions.factorial_operation(1), 1)
     self.assertEqual(MathFunctions.factorial_operation(5), 120)
     self.assertEqual(MathFunctions.factorial_operation(10), 3628800)
 
-  def test_non_intiger_fac(self):  # testing if factorial of non intiger numbers (float, double) raises error
+  ##
+  # @brief testing if factorial of non intiger numbers (float, double) raises error
+  #
+  def test_non_intiger_fac(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.factorial_operation(1.1)
     with self.assertRaises(RuntimeError):
@@ -137,7 +175,10 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(RuntimeError):
       MathFunctions.factorial_operation(435.854)
 
-  def test_neg_number_fac(self):  # testing if factorial of negative numbers raises error
+  ##
+  # @brief testing if factorial of negative numbers raises error
+  #
+  def test_neg_number_fac(self):  
     with self.assertRaises(RuntimeError):
       MathFunctions.factorial_operation(-1)
     with self.assertRaises(RuntimeError):
@@ -145,14 +186,20 @@ class MathLibTestBasicFunctions(unittest.TestCase):
     with self.assertRaises(RuntimeError):
       MathFunctions.factorial_operation(-8.3)
 
-  def test_inv(self):  # inversion function tests
+  ##
+  # @brief inversion function tests
+  #
+  def test_inv(self):  
     self.assertEqual(MathFunctions.invert_operation(0), 0)
     self.assertEqual(MathFunctions.invert_operation(1), -1)
     self.assertEqual(MathFunctions.invert_operation(-1), 1)
     self.assertEqual(MathFunctions.invert_operation(50), -50)
     self.assertEqual(MathFunctions.invert_operation(-100.25), 100.25)
 
-  def test_abs(self): # absolute value function tests
+  ##
+  # @brief absolute value function tests
+  #
+  def test_abs(self): 
     self.assertEqual(MathFunctions.abs_operation(0), 0)
     self.assertEqual(MathFunctions.abs_operation(1), 1)
     self.assertEqual(MathFunctions.abs_operation(1.5), 1.5)
@@ -572,13 +619,188 @@ class MathLibTestParser(unittest.TestCase):
 # @brief Testing Interpreter class of math library
 #
 class MathLibTestInterpreter(unittest.TestCase):
-  pass
+  def setUp(self) -> None:
+    self.interpreter = Interpreter()
+
+  ##
+  # @brief Testing interpretation of single numbers
+  #   
+  def test_numbers(self):
+    self.assertEqual(self.interpreter.interpret(NumberNode(0)).get_value(), 0)
+    self.assertEqual(self.interpreter.interpret(NumberNode(5)).get_value(), 5)
+    self.assertEqual(self.interpreter.interpret(NumberNode(-5)).get_value(), -5)
+    self.assertEqual(self.interpreter.interpret(NumberNode(-5.5)).get_value(), -5.5)
+    self.assertEqual(self.interpreter.interpret(NumberNode(5555555555)).get_value(), 5555555555)
+
+  ##
+  # @brief Testing interpretation of unary operations
+  #
+  def test_unary(self):
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.PLUS), NumberNode(6))).get_value(), 6)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.MINUS), NumberNode(6))).get_value(), -6)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.PLUS), NumberNode(-7.5))).get_value(), -7.5)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.MINUS), NumberNode(-8.6))).get_value(), 8.6)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "abs"), NumberNode(15))).get_value(), 15)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "abs"), NumberNode(-33.3))).get_value(), 33.3)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "fact"), NumberNode(5))).get_value(), 120)
+    self.assertEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "fact"), NumberNode(10))).get_value(), 3628800)
+    self.assertAlmostEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "ln"), NumberNode(10))).get_value(), 2.30258509299, 10)
+    self.assertAlmostEqual(self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "ln"), NumberNode(15.6))).get_value(), 2.74727091426, 10)
+  
+  ##
+  # @brief Testing interpretation of binary operations
+  #
+  def test_binary(self):
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(1), Token(TokenType.PLUS), NumberNode(1))).get_value(), 2)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(71.3), Token(TokenType.PLUS), NumberNode(28.7))).get_value(), 100)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(10), Token(TokenType.MINUS), NumberNode(15))).get_value(), -5)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(15), Token(TokenType.MINUS), NumberNode(10))).get_value(), 5)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(10), Token(TokenType.DIVIDE), NumberNode(5))).get_value(), 2)
+    self.assertAlmostEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(55), Token(TokenType.DIVIDE), NumberNode(43))).get_value(), 1.27906976744, 10)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(3), Token(TokenType.MULTIPLY), NumberNode(4))).get_value(), 12)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(0), Token(TokenType.MULTIPLY), NumberNode(6000))).get_value(), 0)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(3), Token(TokenType.POW), NumberNode(0))).get_value(), 1)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(2), Token(TokenType.POW), NumberNode(10))).get_value(), 1024)
+    self.assertEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(2), Token(TokenType.ROOT), NumberNode(4))).get_value(), 2)
+    self.assertAlmostEqual(self.interpreter.interpret(BinaryOperationNode(NumberNode(3), Token(TokenType.ROOT), NumberNode(5))).get_value(), 1.70997594668, 10)
+  
+  ##
+  # @brief Testing if interpretation of invalid function inputs raises error
+  #
+  def test_invalid_inputs(self):
+    with self.assertRaises(ZeroDivisionError):
+      self.interpreter.interpret(BinaryOperationNode(NumberNode(10), Token(TokenType.DIVIDE), NumberNode(0)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(BinaryOperationNode(NumberNode(0), Token(TokenType.ROOT), NumberNode(15)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(BinaryOperationNode(NumberNode(2), Token(TokenType.ROOT), NumberNode(-3)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "fact"), NumberNode(1.1)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "fact"), NumberNode(-1)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "ln"), NumberNode(0)))
+    with self.assertRaises(RuntimeError):
+      self.interpreter.interpret(UnaryOperationNode(Token(TokenType.KEYWORD, "ln"), NumberNode(-1)))
+
+
+
 
 ##
 # @brief Testing entrypoint for UI
 #
 class MathLibTestExpressions(unittest.TestCase):
-  pass
+
+  ##
+  # @brief testing interpretation of add function
+  #
+  def test_int_add(self):
+    self.assertEqual(interpret_text_input("0 + 0"),  "0")
+    self.assertEqual(interpret_text_input("10 + 5"),  "15")
+    self.assertEqual(interpret_text_input("10.5 + 5.35"),  "15.85")
+    self.assertEqual(interpret_text_input("485.477 + 7741.999"),  "8227.476")
+  
+  ##
+  # @brief testing interpretation of subtraction function
+  #
+  def test_int_sub(self):
+    self.assertEqual(interpret_text_input("0 - 0"), "0")
+    self.assertEqual(interpret_text_input("5 - 4"), "1")
+    self.assertEqual(interpret_text_input("4 - 5"), "-1")
+    self.assertEqual(interpret_text_input("15.2 - 16.2"), "-1")
+    self.assertEqual(interpret_text_input("7855.445 - 889.51"), "6965.935")
+  
+  ##
+  # @brief testing interpretation of multiply function
+  #
+  def test_int_mul(self):
+    self.assertEqual(interpret_text_input("0 * 0"), "0")
+    self.assertEqual(interpret_text_input("5 * 0"), "0")
+    self.assertEqual(interpret_text_input("7 * 8"), "56")
+    self.assertEqual(interpret_text_input("33.5 * 74.4"), "2492.4")
+    self.assertEqual(interpret_text_input("456.789 * 987.654"), "451149.483006")
+  
+  ##
+  # @brief testing interpretation of division function
+  #
+  def test_int_div(self):
+    self.assertEqual(interpret_text_input("0 / 5"), "0")
+    self.assertEqual(interpret_text_input("5 / 5"), "1")
+    self.assertEqual(interpret_text_input("14 / 5"), "2.8")
+    self.assertAlmostEqual(float(interpret_text_input("45.3 / 56")), float("0.8089285714"), 9)
+  
+  ##
+  # @brief testing interpretation of power function
+  #
+  def test_int_pow(self):
+    self.assertEqual(interpret_text_input("1^0"), "1")
+    self.assertEqual(interpret_text_input("2^1"), "2")
+    self.assertEqual(interpret_text_input("5^6"), "15625")
+    self.assertAlmostEqual(float(interpret_text_input("4.5^5.6")), 4549.77664599199, 10)
+    self.assertAlmostEqual(float(interpret_text_input("451.1^2.4")), 2345604.49398618945, 10)
+  
+  ##
+  # @brief testing interpretation of root function
+  #
+  def test_int_root(self):
+    self.assertEqual(interpret_text_input("1√5"), "5")
+    self.assertAlmostEqual(float(interpret_text_input("2√5")), 2.2360679774, 10)
+    self.assertAlmostEqual(float(interpret_text_input("5√5")), 1.37972966146, 10)
+    self.assertAlmostEqual(float(interpret_text_input("15√452.45")), 1.50327363084, 10)
+  
+  ##
+  # @brief testing interpretation of natural logarithm function
+  #
+  def test_int_ln(self):
+    self.assertEqual(interpret_text_input("ln 1"), "0")
+    self.assertAlmostEqual(float(interpret_text_input("ln 5")), 1.60943791243, 10)
+    self.assertAlmostEqual(float(interpret_text_input("ln 5,3")), 1.66770682055, 10)
+    self.assertAlmostEqual(float(interpret_text_input("ln 789.789")), 6.67176582117, 10)
+
+  ##
+  # @brief testing interpretation of absolute value function
+  #
+  def test_int_abs(self):
+    self.assertEqual(interpret_text_input("abs 0"), "0")
+    self.assertEqual(interpret_text_input("abs 1"), "1")
+    self.assertEqual(interpret_text_input("abs -1"), "1")
+    self.assertEqual(interpret_text_input("abs -485.756"), "485.756")
+  ##
+  # @brief testing interpretation of factorial function
+  #
+  def test_int_fact(self):
+    self.assertEqual(interpret_text_input("fact 1"), "1")
+    self.assertEqual(interpret_text_input("fact 2"), "2")
+    self.assertEqual(interpret_text_input("fact 5"), "120")
+    self.assertEqual(interpret_text_input("fact 13"), "6227020800")
+  
+  ##
+  # @brief testing interpretation of multiple functions
+  #
+  def test_int_multiple(self):
+    self.assertEqual(interpret_text_input("5 + 5 - 4 / 6 + 8 * 4"), "41.33333333333") 
+    self.assertEqual(interpret_text_input("abs -5^5 - 4 / 6√8 * fact 4"), "3057.117") 
+  
+  ##
+  # @brief testing violating mathematical rules 
+  #
+  def test_int_violation(self):
+    self.assertEqual(interpret_text_input("4 / 0"), "Error")
+    self.assertEqual(interpret_text_input("0√10"), "Error")
+    self.assertEqual(interpret_text_input("2√-78"), "Error")
+    self.assertEqual(interpret_text_input("fact -5"), "Error")
+    self.assertEqual(interpret_text_input("fact 55.5"), "Error")
+    self.assertEqual(interpret_text_input("ln 0"), "Error")
+    self.assertEqual(interpret_text_input("ln -30"), "Error")
+  ##
+  # @brief testing syntax
+  #
+  def test_int_syntax(self):
+    self.assertEqual(interpret_text_input("qqqqqqqqqqqqqq"), "Error")
+    self.assertEqual(interpret_text_input("5////////4"), "Error")
+    self.assertEqual(interpret_text_input("14a+sa"), "Error")
+    self.assertEqual(interpret_text_input("())fact (5)1(((asd)"), "Error")
+    self.assertEqual(interpret_text_input("fact ln abs √^+-/*"), "Error")
 
 if __name__ == '__main__':
   try:
